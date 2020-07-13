324015eda984f2dada7d23d6c07416b20216c5b8
TelerikArsov <telerik007@gmail.com>
2020-07-13 18:42:45 +0300

    Added own mysql insert and metrics

--- telebid-map/insert-marker.php
+++ telebid-map/insert-marker.php
@@ -1,4 +1,12 @@
 <?php
+global $first_time, $conn, $stmt, $servername, $username, $password, $dbname;
+$servername = "localhost";
+$username = "";
+$password = ""; #own
+$dbname = "";
+$first_time = 1;
+$conn = null;
+$stmt = null;
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if (! isset($_POST['nonce_marker']) || ! wp_verify_nonce( $_POST['nonce_marker'], 'add_marker' )) {
@@ -6,7 +14,6 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
          return;
     }
     if(isset($_POST['alot'])) {
-        echo "SAVING A LOT";
         insert_alot();
     }else{
         insert_row($_POST['location_name'], $_POST['lat'], $_POST['lng']);
@@ -17,23 +24,57 @@ function insert_alot(){
     if(!isset($ammount) || trim($ammount) == ''){
         echo 'VEry wrong';
     }else {
+        $start = microtime(true);
         for($i = 0; $i < intval($ammount); $i++){
-            $json = file_get_contents("https://api.3geonames.org/?randomland=yes&json=1");
+            $json = get_coords('default');
             $json = json_decode($json);
             $lat = $json->{'nearest'}->{'latt'};
             $lng = $json->{'nearest'}->{'longt'};
             $name = $json->{'threegeonames'};
             insert_row($name, $lat, $lng);
         }
+        echo $time_elapsed_secs = microtime(true) - $start;
+        global $conn, $stmt;
+        if (isset($conn) && mysqli_ping($conn)){
+            $stmt->close();
+            $conn->close();
+        }
     }
 }
+
+function get_coords($type)
+{
+    $data = null;
+    if($type == 'wp-get'){
+        $data = wp_remote_get('https://api.3geonames.org/?randomland=yes&json=1')['body'];
+    }elseif($type == 'default'){
+        $data = file_get_contents('https://api.3geonames.org/?randomland=yes&json=1');
+    }
+    return $data;
+}
+
+function insert_row_raw($name, $lat, $lng){
+    global $first_time, $stmt, $conn;
+    if($first_time == 1) {
+        global $servername, $username, $password, $dbname;
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        }
+        $stmt = $conn->prepare("INSERT INTO wp_markers (name, lat, lng) VALUES (?, ?, ?)");
+        $stmt->bind_param("sdd", $name, $lat, $lng);
+        $first_time = 0;
+    }
+    $stmt->execute();  
+}
+
 function insert_row($name, $lat, $lng) {
     global $wpdb;
     $tblname = 'markers';
     $wp_track_table = $wpdb->prefix. "$tblname";
     if(!isset($name) || trim($name) == ''
-    || !isset($lat) || trim($lat) == ''
-    || !isset($lng) || trim($lng) == '')
+    || !isset($lat) || (is_string($lat) && trim($lat)) == ''
+    || !isset($lng) || (is_string($lng) && trim($lng)) == '')
     {
         echo "Wrong input";
     }
