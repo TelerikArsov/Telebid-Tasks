35382a4eb5a9261d785b6792001bf46c77bd94ea
TelerikArsov <telerik007@gmail.com>
2020-07-14 14:47:30 +0300

    Added marker filter option.

--- telebid-map/views/map-view.php
+++ telebid-map/views/map-view.php
@@ -4,14 +4,23 @@
         height: 900px;
         background-color: grey;
     }
+    strong, text{
+        color: black;
+    }
+    strong#title {
+        font-size: 15px;
+    }
 </style>
 <h3>Maps</h3>
+<select id="filterOptions"><br><br>
+<input type="text" id="filter" name="fname"><br><br>
+
 <div id="map"></div>
 <script> 
     map; 
     function initMap() { 
-        map = new google.maps.Map(document.getElementById('map'), { center: { lat: -33.863276, lng: 151.207977 }, zoom: 8 });
+        map = new google.maps.Map(document.getElementById('map'), { center: { lat: -33.863276, lng: 151.207977 }, zoom: 4 });
     }   
 </script>
-<script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY&callback=initMap" async
+<script src="https://maps.googleapis.com/maps/api/js?key=&callback=initMap" async
     defer></script>
