ecfaa09dff8a2b0cbd17e9b5b8ef90e52f1e48bf
TelerikArsov <telerik007@gmail.com>
2020-07-17 15:18:31 +0300

    Iserting data to db from xlsx files with hardcoded paths.

--- ekatte/base_schema.sql
+++ ekatte/base_schema.sql
@@ -1,18 +1,17 @@
-create table settlements (
-    ekatte_id INTEGER PRIMARY KEY,                         --Unique id of settlement
-    name VARCHAR(25) NOT NULL,                             --Name of settlement
-    t_v_m VARCHAR(4) NOT NULL,                             --Prefix of name
-    muni_code VARCHAR(5) REFERENCES municipalities(code)   --Municipality to which the settlement belongs
-);
-
 create table areas (
-    code VARCHAR(3) PRIMARY KEY,                                --Unique code of area
+    id VARCHAR(3) PRIMARY KEY,                                --Unique code of area
     name VARCHAR(25) NOT NULL,                                  --Name of area
     region VARCHAR(4) NOT NULL                                  --Region of level 2 to which the area belong                                   
 );
 
 create table municipalities (
-    code VARCHAR(3) PRIMARY KEY,                                --Unique code of municipality                
+    id VARCHAR(5) PRIMARY KEY,                                --Unique code of municipality                
     name VARCHAR(25) NOT NULL,                                  --Name of municipality
-    area_code VARCHAR(3) REFERENCES areas(code)              --Area to which the municipality belongs
+    area_id VARCHAR(3) REFERENCES areas(id)              --Area to which the municipality belongs
+);
+create table settlements (
+    id INTEGER PRIMARY KEY,                         --Unique id of settlement
+    name VARCHAR(25) NOT NULL,                             --Name of settlement
+    t_v_m VARCHAR(4) NOT NULL,                             --Prefix of name
+    muni_id VARCHAR(5) REFERENCES municipalities(id)   --Municipality to which the settlement belongs
 );
---
+++ ekatte/populate_base_test.py
@@ -0,0 +1,93 @@
+from dotenv import load_dotenv
+load_dotenv("variables.env")
+
+import psycopg2
+import os
+
+import pandas as pd
+dfs = pd.read_excel('/home/telerik/Downloads/Ekatte/Ekatte_xlsx/Ek_obl.xlsx', usecols=['oblast', 'name', 'region'])
+area = '/home/telerik/Downloads/Ekatte/Ekatte_xlsx/Ek_obl.xlsx'
+sett = '/home/telerik/Downloads/Ekatte/Ekatte_xlsx/Ek_atte.xlsx'
+muni = '/home/telerik/Downloads/Ekatte/Ekatte_xlsx/Ek_obst.xlsx'
+def get_connection():
+    connection = psycopg2.connect(user = os.environ.get("db-username"),
+                                  password = os.environ.get("db-password"),
+                                  host = os.environ.get("db-host"),
+                                  port = os.environ.get("db-port"),
+                                  database = os.environ.get("db"))
+    return connection 
+
+def close_connection(connection):
+    if connection:
+        connection.close()
+        print("Postgres connection is closed")
+
+def create_area(connection, area_path):
+    cols = ['oblast', 'name','region']
+    df = pd.read_excel(area_path, usecols=cols)
+    cursor = connection.cursor()
+    postgres_insert_query = """ INSERT INTO areas (ID, NAME, REGION) VALUES (%s,%s,%s)"""
+    for _, row in df.iterrows():
+        try:
+            if not record_exists(cursor, 'areas', row[cols[0]]):
+                cursor.execute(postgres_insert_query, (row[cols[0]], row[cols[1]], row[cols[2]]))
+        except (Exception, psycopg2.Error) as error :
+            print("Error while inserting into area", error)
+    
+    connection.commit()
+    cursor.close()
+
+def record_exists(cursor, table_name, id_value):
+    from psycopg2.extensions import AsIs
+    check_id_exists_query = """ SELECT id FROM %s WHERE id = %s """
+    cursor.execute(check_id_exists_query, (AsIs(table_name), id_value))
+    return cursor.fetchone() is not None
+
+def create_muni_sett(connection, muni_path, sett_path):
+    muni_cols = ['obstina', 'name']
+    muni_df = pd.read_excel(muni_path, usecols=muni_cols)
+    muni_insert_query = """ INSERT INTO municipalities (ID, NAME, AREA_ID) VALUES (%s, %s, %s) """
+    sett_cols = ['ekatte', 'name', 't_v_m', 'oblast', 'obstina']
+    sett_df = pd.read_excel(sett_path, usecols=sett_cols)
+    sett_insert_query = """ INSERT INTO settlements (ID, NAME, T_V_M, MUNI_ID) VALUES (%s, %s, %s, %s) """
+
+
+    cursor = connection.cursor()
+    skip_first = True
+    for _, sett_row in sett_df.iterrows():
+        if skip_first:
+            skip_first = False
+            continue
+        try:
+            if not record_exists(cursor, 'municipalities', sett_row[sett_cols[4]]):
+                muni = muni_df.loc[muni_df[muni_cols[0]] == sett_row[sett_cols[4]]]
+                cursor.execute(muni_insert_query, (muni.iloc[0][muni_cols[0]], muni.iloc[0][muni_cols[1]],  sett_row[sett_cols[3]]))
+            #else:
+            #    print("ignored entry. Already exists")
+            if not record_exists(cursor, 'settlements', sett_row[sett_cols[0]]):
+                cursor.execute(sett_insert_query, (sett_row[sett_cols[0]], sett_row[sett_cols[1]], sett_row[sett_cols[2]], sett_row[sett_cols[4]]))
+            #else:
+            #    print("Ignored settlement. Already exists")
+            connection.commit()
+        except (Exception, psycopg2.Error) as error:
+            print("Error while inserting settlements", error)
+            connection.rollback()
+    cursor.close()
+
+
+
+def insert_data(area_path, muni_path, sett_path):
+    try:
+        connection = get_connection()
+        create_area(connection, area_path)
+        create_muni_sett(connection, muni_path, sett_path)
+    except (Exception, psycopg2.Error) as error :
+        print ("Error while connecting to PostgreSQL", error)
+    finally:
+        #closing database connection.
+        close_connection(connection)
+try:
+    insert_data(area, muni, sett)
+
+except (Exception, psycopg2.Error) as error :
+    print ("Error while connecting to PostgreSQL", error)
