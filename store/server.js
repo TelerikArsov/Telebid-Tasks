3b3553768eb34d07a9a3418a916435d06e349393
TelerikArsov <telerik007@gmail.com>
2020-07-21 18:43:25 +0300

    Added postgres module

--- store/server.js
+++ store/server.js
@@ -1,6 +1,20 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = 5000;
+
+const { Client } = require('pg');
+
+const client = new Client({
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: process.env.PGPORT,
+});
+
+client.connect();
+
 
 app.listen(port, () => console.log(`Server started on port: ${port}`));
 
