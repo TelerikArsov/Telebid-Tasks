319dee3ed6e8c0fc3f4a1abdfdb8ffcfa4e6b095
TelerikArsov <telerik007@gmail.com>
2020-07-21 18:44:39 +0300

    Hello world test on pg db.

--- store/server.js
+++ store/server.js
@@ -14,7 +14,10 @@ const client = new Client({
 });
 
 client.connect();
-
+client.query('SELECT $1::text as message', ['Hello world!'], (err, res) => {
+    console.log(err ? err.stack : res.rows[0].message) // Hello World!
+    client.end()
+})
 
 app.listen(port, () => console.log(`Server started on port: ${port}`));
 
